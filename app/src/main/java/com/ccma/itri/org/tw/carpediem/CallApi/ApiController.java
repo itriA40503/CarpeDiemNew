7dc7b74b94ab711cb400c6ebdfbf3ae540d979ba
A40503 <itriA40503@itri.org.tw>
2016-09-29 13:16:16 +0800

    Add API

---
+++ app/src/main/java/com/ccma/itri/org/tw/carpediem/CallApi/ApiController.java
@@ -0,0 +1,52 @@
+package com.ccma.itri.org.tw.carpediem.CallApi;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Observable;
+
+/**
+ * Created by A40503 on 2016/9/26.
+ */
+public class ApiController {
+    Gson gson = new GsonBuilder()
+            .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+            .create();
+    public final Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(CarpeDiemApi.ENDPOINT)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+            .build();
+
+    public static ApiController mInstance;
+    public static ApiController getInstance(){
+        if (mInstance == null){
+            mInstance = new ApiController();
+        }
+        return mInstance;
+    }
+
+    public CarpeDiemApi mAPI;
+
+    public ApiController(){
+        mAPI = retrofit.create(CarpeDiemApi.class);
+    }
+
+    public Observable<CarpeDiemObject>getToken(String uuid){
+//        return mAPI.getTokenOb(uuid);
+        return mAPI.getTokenGET(uuid);
+    }
+
+    public Observable<CarpeDiemObject>sigin(String uuid){
+        return mAPI.getSiginByUUID(uuid);
+    }
+
+    public Observable<CarpeDiemListEventObject>getEventList(String token){
+        return mAPI.getEventList(token);
+    }
+}
