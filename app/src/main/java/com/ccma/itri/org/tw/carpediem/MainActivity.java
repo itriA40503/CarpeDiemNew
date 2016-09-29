aa5c6082365d0bf6a802a9bbd15aed8300375eb6
A40503 <itriA40503@itri.org.tw>
2016-09-29 17:49:53 +0800

    MainActivity only do pushNewFragment

--- app/src/main/java/com/ccma/itri/org/tw/carpediem/MainActivity.java
+++ app/src/main/java/com/ccma/itri/org/tw/carpediem/MainActivity.java
@@ -5,7 +5,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.ccma.itri.org.tw.carpediem.EventObject.TimeEvent;
 import com.ccma.itri.org.tw.carpediem.UserData.UserData;
+
+import java.util.ArrayList;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     private TextView text;
@@ -18,8 +26,41 @@ public class MainActivity extends AppCompatActivity {
         text = (TextView)findViewById(R.id.textTest);
         text.setText(getUserDataManager().getUserToken());
 
+//        Log.d("getUUI",getUserDataManager().getUserUUID());
+//        CarpeDiemController.getInstance().Events = new ArrayList<TimeEvent>();
+//        CarpeDiemController.getInstance().Events.add(new TimeEvent("FisrtEvent", 5000, false));
+//        CarpeDiemController.getInstance().Events.add(new TimeEvent("SecondEvent", 10000, false));
+//        CarpeDiemController.getInstance().Events.add(new TimeEvent("ContinuousEvent", 10000, true));
+//        CarpeDiemController.getInstance().Events.add(new TimeEvent("ContinuousEvent2", 5000, true));
 //        getUserDataManager().clearUserData();
         pushNewFragment(new CardFragment());
+        //# pushNewFragment After Get EventList
+//        observable.subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(new Subscriber<CarpeDiemController>() {
+//                    @Override
+//                    public void onCompleted() {
+//                        pushNewFragment(new CardFragment());
+//                    }
+//
+//                    @Override
+//                    public void onError(Throwable e) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onNext(CarpeDiemController carpeDiemController) {
+////                        carpeDiemController.RxGetEventList(getUserDataManager().getUserToken());
+//                    }
+//
+////                    @Override
+////                    public void onNext(String s) {
+////                        //# Get Event List
+////                        CarpeDiemController.getInstance().RxGetEventList(getUserDataManager().getUserToken());
+////                        Log.d("observable",s);
+////
+////                    }
+//                });
 //        Log.d("mainGet",UserData.getInstance().getUserUUID());
     }
     protected UserData getUserDataManager() {
@@ -30,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void pushNewFragment(android.app.Fragment fragment){
+        Log.d("pushNewFragment","fragment");
         fragmentMgr = getFragmentManager();
         android.app.FragmentTransaction fragmentTrans = fragmentMgr.beginTransaction();
         fragmentTrans.replace(R.id.fragment, fragment);
@@ -37,4 +79,22 @@ public class MainActivity extends AppCompatActivity {
 //        fragmentTrans.replace(R.id.frameLayout, fragment);
         fragmentTrans.commit();
     }
+
+    Observable observable = Observable.create(new Observable.OnSubscribe<CarpeDiemController>() {
+        @Override
+        public void call(Subscriber<? super CarpeDiemController> subscriber) {
+            CarpeDiemController carpeDiemController = CarpeDiemController.getInstance();
+            subscriber.onNext(carpeDiemController);
+            subscriber.onCompleted();
+        }
+
+//        @Override
+//        public void call(Subscriber<? super String> subscriber) {
+//            subscriber.onNext("Get Event List");
+//            //# Get Event List
+//            CarpeDiemController.getInstance().RxGetEventList(getUserDataManager().getUserToken());
+//            subscriber.onNext("Get Event List END");
+//            subscriber.onCompleted();
+//        }
+    });
 }
