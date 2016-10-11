382fa142f1156f073e47ea97d8631a53494e4118
A40503 <itriA40503@itri.org.tw>
2016-10-11 15:03:52 +0800

    add ApiGeneralResult, (int)code, (int)getCode

---
+++ app/src/main/java/com/ccma/itri/org/tw/carpediem/CallApi/ApiGeneralResult.java
@@ -0,0 +1,12 @@
+package com.ccma.itri.org.tw.carpediem.CallApi;
+
+/**
+ * Created by A40503 on 2016/10/11.
+ */
+
+public class ApiGeneralResult<T> {
+    public int code;
+    public int getCode(){
+        return  code;
+    }
+}
