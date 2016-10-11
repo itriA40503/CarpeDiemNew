a1a1eba974fb7402a7cde57b85d136378a858e5c
A40503 <itriA40503@itri.org.tw>
2016-10-11 15:14:52 +0800

    update

--- app/src/main/java/com/ccma/itri/org/tw/carpediem/CardFragment.java
+++ app/src/main/java/com/ccma/itri/org/tw/carpediem/CardFragment.java
@@ -7,7 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-
+import android.support.v4.app.Fragment;
 import com.ccma.itri.org.tw.carpediem.Adapter.TimeEventAdapter;
 import com.ccma.itri.org.tw.carpediem.EventObject.TimeEvent;
 
@@ -16,7 +16,7 @@ import java.util.List;
 /**
  * Created by A40503 on 2016/9/21.
  */
-public class CardFragment extends android.app.Fragment {
+public class CardFragment extends Fragment {
 
     private ListView listEvent;
     private List<TimeEvent> timeEventList;
@@ -26,11 +26,13 @@ public class CardFragment extends android.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         Log.d("CardFragment","onCreateView");
         View view = inflater.inflate(R.layout.fragment_main, container, false);
-
-        listEvent = (ListView)view.findViewById(R.id.lv_time_event);
         timeEventList = CarpeDiemController.getInstance().getTimeEvents();
+        listEvent = (ListView)view.findViewById(R.id.lv_time_event);
+        Log.d("CardFragment","new TimeEventAdapter");
         mTimeEventAdapter = new TimeEventAdapter(getActivity());
+        Log.d("CardFragment","setAdapter");
         listEvent.setAdapter(mTimeEventAdapter);
+        Log.d("CardFragment","timeEventList");
         mTimeEventAdapter.setItems(timeEventList);
 
         return view;
@@ -40,6 +42,7 @@ public class CardFragment extends android.app.Fragment {
     public void onPause() {
         super.onPause();
         Log.d("CardFragment","onPause");
+        mTimeEventAdapter.notifyDataSetChanged();
 //        listEvent.setAdapter(mTimeEventAdapter);
 //        mTimeEventAdapter.setItems(timeEventList);
     }
@@ -48,7 +51,8 @@ public class CardFragment extends android.app.Fragment {
     public void onResume() {
         super.onResume();
         Log.d("CardFragment","onResume");
-        listEvent.setAdapter(mTimeEventAdapter);
-        mTimeEventAdapter.setItems(timeEventList);
+        mTimeEventAdapter.notifyDataSetChanged();
+//        listEvent.setAdapter(mTimeEventAdapter);
+//        mTimeEventAdapter.setItems(timeEventList);
     }
 }
