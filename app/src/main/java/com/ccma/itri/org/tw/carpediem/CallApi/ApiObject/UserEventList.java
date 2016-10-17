5dfadd5d7f981368c3026d02ee030a465cfa55ab
A40503 <itriA40503@itri.org.tw>
2016-10-17 16:46:37 +0800

    add UserEventList for api

---
+++ app/src/main/java/com/ccma/itri/org/tw/carpediem/CallApi/ApiObject/UserEventList.java
@@ -0,0 +1,19 @@
+package com.ccma.itri.org.tw.carpediem.CallApi.ApiObject;
+
+/**
+ * Created by A40503 on 2016/10/17.
+ */
+
+public class UserEventList {
+    String id;
+    String eventId;
+    String createdAt;
+
+    public String getId(){
+        return id;
+    }
+
+    public String getEventId(){
+        return eventId;
+    }
+}
