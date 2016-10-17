792b864c9d30e35e4eb2223503875653a39191b2
A40503 <itriA40503@itri.org.tw>
2016-10-17 16:46:13 +0800

    add EventLists object for api

---
+++ app/src/main/java/com/ccma/itri/org/tw/carpediem/CallApi/ApiObject/EventLists.java
@@ -0,0 +1,14 @@
+package com.ccma.itri.org.tw.carpediem.CallApi.ApiObject;
+
+import com.ccma.itri.org.tw.carpediem.CallApi.CarpeDiemEventObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by A40503 on 2016/10/17.
+ */
+
+public class EventLists {
+    public ArrayList<CarpeDiemEventObject> eventList;
+    public ArrayList<UserEventList>userEventList;
+}
