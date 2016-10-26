fe8c69b4669c791bd954a4bda8a08488288e730b
A40503 <itriA40503@itri.org.tw>
2016-10-26 16:44:04 +0800

    add ArrayUserItemList

---
+++ app/src/main/java/com/ccma/itri/org/tw/carpediem/CallApi/ApiObject/ArrayUserItemList.java
@@ -0,0 +1,11 @@
+package com.ccma.itri.org.tw.carpediem.CallApi.ApiObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by A40503 on 2016/10/24.
+ */
+
+public class ArrayUserItemList {
+    public ArrayList<UserItemList> userItemList;
+}
