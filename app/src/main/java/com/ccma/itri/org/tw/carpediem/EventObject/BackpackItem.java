b9abbcf09d83c4ba2eb21ca97f338c343b4aec9b
A40503 <itriA40503@itri.org.tw>
2016-11-02 16:18:47 +0800

    add backpackItem

---
+++ app/src/main/java/com/ccma/itri/org/tw/carpediem/EventObject/BackpackItem.java
@@ -0,0 +1,16 @@
+package com.ccma.itri.org.tw.carpediem.EventObject;
+
+/**
+ * Created by A40503 on 2016/11/2.
+ */
+
+public class BackpackItem {
+    private static final String TAG = "BackpackItem";
+    public String mName, mDescription;
+
+    public BackpackItem (String name, String description){
+        mName = name;
+        mDescription = description;
+    }
+
+}
