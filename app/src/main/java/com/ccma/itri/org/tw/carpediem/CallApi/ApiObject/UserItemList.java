49624216e2ff73e2cb44af18a828fbc78556b1f0
A40503 <itriA40503@itri.org.tw>
2016-11-10 16:54:32 +0800

    add getCreatedAt

--- app/src/main/java/com/ccma/itri/org/tw/carpediem/CallApi/ApiObject/UserItemList.java
+++ app/src/main/java/com/ccma/itri/org/tw/carpediem/CallApi/ApiObject/UserItemList.java
@@ -1,5 +1,8 @@
 package com.ccma.itri.org.tw.carpediem.CallApi.ApiObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by A40503 on 2016/10/24.
  */
@@ -23,6 +26,25 @@ public class UserItemList {
         return item;
     }
 
+    public String getCreatedAt(){
+        Double doubleNum = Double.parseDouble(createdAt);
+        long unixSeconds = doubleNum.longValue();
+        Date date = new Date(unixSeconds*1000L); // *1000 is to convert seconds to milliseconds
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd"); // the format of your date
+    //        sdf.setTimeZone(TimeZone.getTimeZone("GMT-4")); // give a timezone reference for formating (see comment at the bottom
+        String formattedDate = sdf.format(date);
+        return formattedDate;
+    }
+
+    public String getExpiredAt(){
+        Double doubleNum = Double.parseDouble(expiredAt);
+        long unixSeconds = doubleNum.longValue();
+        Date date = new Date(unixSeconds*1000L); // *1000 is to convert seconds to milliseconds
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd"); // the format of your date
+//        sdf.setTimeZone(TimeZone.getTimeZone("GMT-4")); // give a timezone reference for formating (see comment at the bottom
+        String formattedDate = sdf.format(date);
+        return formattedDate;
+    }
    public class item{
        String itemId;
        String typeId;
@@ -44,5 +66,7 @@ public class UserItemList {
        public String getItemDesc(){
            return description;
        }
+
+
    }
 }
