72ff462c1dc4524249508eab9d95cb5a86a20118
A40503 <itriA40503@itri.org.tw>
2016-11-18 16:36:14 +0800

    add ApiObject Image

---
+++ app/src/main/java/com/ccma/itri/org/tw/carpediem/CallApi/ApiObject/Image.java
@@ -0,0 +1,11 @@
+package com.ccma.itri.org.tw.carpediem.CallApi.ApiObject;
+
+/**
+ * Created by A40503 on 2016/11/16.
+ */
+
+public class Image {
+    String url;
+
+    public String getUrl(){return url;}
+}
