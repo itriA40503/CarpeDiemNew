1558b180c009eedb5371050f07dcb6bda848befd
A40503 <itriA40503@itri.org.tw>
2016-11-21 08:46:31 +0800

    add RewardItem constructor

--- app/src/main/java/com/ccma/itri/org/tw/carpediem/EventObject/RewardItem.java
+++ app/src/main/java/com/ccma/itri/org/tw/carpediem/EventObject/RewardItem.java
@@ -1,5 +1,7 @@
 package com.ccma.itri.org.tw.carpediem.EventObject;
 
+import com.ccma.itri.org.tw.carpediem.CallApi.ApiObject.Advertiser;
+
 /**
  * Created by A40503 on 2016/11/9.
  */
@@ -9,12 +11,28 @@ public class RewardItem {
     String mTypeId;
     String mName;
     String mDescription;
-    public RewardItem (String itemId, String typeId, String name, String descrpition){
+    String mAdvertiserId;
+    Advertiser mAdvertiser;
+
+    public RewardItem (String itemId, String typeId, String name, String descrpition, String advertiserId){
         mItemId = itemId;
         mTypeId = typeId;
         mName = name;
         mDescription = descrpition;
+//        mAdvertiser = advertiser;
+        mAdvertiserId = advertiserId;
     }
+
+    public RewardItem (String itemId, String typeId, String name, String descrpition, Advertiser advertiser){
+        mItemId = itemId;
+        mTypeId = typeId;
+        mName = name;
+        mDescription = descrpition;
+        mAdvertiser = advertiser;
+//        mAdvertiserId = AdvertiserId;
+    }
+
+    public Advertiser getAdvertiser(){return mAdvertiser;}
 
     public String getName(){return mName;}
 
