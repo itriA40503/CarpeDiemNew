691ba40cdc4c7fbb2de10017899d042ffee93e15
A40503 <itriA40503@itri.org.tw>
2016-11-21 08:48:33 +0800

    add logo setting

--- app/src/main/java/com/ccma/itri/org/tw/carpediem/Dialog/BackpackDialog.java
+++ app/src/main/java/com/ccma/itri/org/tw/carpediem/Dialog/BackpackDialog.java
@@ -13,6 +13,7 @@ import com.ccma.itri.org.tw.carpediem.R;
 import com.flyco.animation.FadeEnter.FadeEnter;
 import com.flyco.dialog.utils.CornerUtils;
 import com.flyco.dialog.widget.base.BaseDialog;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by A40503 on 2016/11/10.
@@ -20,7 +21,7 @@ import com.flyco.dialog.widget.base.BaseDialog;
 
 public class BackpackDialog extends BaseDialog<BackpackDialog> {
     private ImageView imgLogo, imgType;
-    private TextView txtTitle, txtContent;
+    private TextView txtTitle, txtContent, txtName;
     private View layout;
     private View inflate;
     private int logo, type;
@@ -53,14 +54,25 @@ public class BackpackDialog extends BaseDialog<BackpackDialog> {
             imgLogo = (ImageView)inflate.findViewById(R.id.img_reward_logo);
             imgType = (ImageView)inflate.findViewById(R.id.img_reward_type);
 
-            imgLogo.setImageResource(logo);
-            imgType.setImageResource(type);
+//            imgLogo.setImageResource(logo);
+//            imgType.setImageResource(type);
+            Picasso.with(mContext).load(type).into(imgType);
+            if(mBackpackItem.getUserItemList().getItem().getImage().getUrl()!=null){
+                Picasso.with(mContext)
+                        .load(mBackpackItem.getUserItemList().getItem().getImage().getUrl())
+                        .resize(300,300)
+                        .into(imgLogo);
+            }else {
+                Picasso.with(mContext).load(logo).into(imgLogo);
+            }
 
             txtTitle = (TextView)inflate.findViewById(R.id.txt_reward_title);
             txtContent = (TextView)inflate.findViewById(R.id.txt_reward_content);
+            txtName = (TextView)inflate.findViewById(R.id.txt_reward_name);
 
             txtTitle.setText(mBackpackItem.getName());
             txtContent.setText(mBackpackItem.getDescription());
+            txtName.setText(mBackpackItem.getAdvertiser().getName());
         }
 //        imageView = (ImageView)inflate.findViewById(R.id.img_dialog);
 //        imageView.setImageResource(mImage);
